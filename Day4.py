b7c74f60f3e3f7df7b70a3569b33b4fdc3293d46
Kwesi Larry <appiahbis999@gmail.com>
2025-01-09 19:44:51 +0000

    Simple countdown program

---
+++ Day4.py
@@ -0,0 +1,9 @@
+# A simple "countdown" program that counts down from 10 to 1 using a while loop.
+# Countdown program
+count = 10
+
+while count > 0:
+    print(count)
+    count -= 1
+
+print("Countdown complete! Thanks for waiting")
