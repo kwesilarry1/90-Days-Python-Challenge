a08fe1cf52ad0d15a2b18cc92269a1630df69279
Kwesi Larry <appiahbis999@gmail.com>
2025-01-11 19:51:40 +0000

    Day6 Task

---
+++ Day6.py
@@ -0,0 +1,17 @@
+#Program that calculates the sum and average of a given numbers
+def calculate_sum_and_average(numbers):
+    if not numbers:
+        print("The list is empty.")
+        return
+    
+    total_sum = sum(numbers)
+    average = total_sum / len(numbers)
+    
+    print(f"Sum: {total_sum}")
+    print(f"Average: {average}")
+
+#Getting User input
+user_input = input("Enter numbers separated by spaces: ")
+numbers_list = [float(num) for num in user_input.split()]
+
+calculate_sum_and_average(numbers_list)
